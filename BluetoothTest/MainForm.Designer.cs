05f1f852a3772d5d4548d0daf45541aef456e9bd
Manuel <wtfjoke@livenet.ch>
2018-05-24 14:21:44 +0200

    rename form1 to mainform

--- BluetoothTest/MainForm.Designer.cs
+++ BluetoothTest/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BluetoothTest
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 475);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Bluetooth reader";
             this.ResumeLayout(false);
 
         }
